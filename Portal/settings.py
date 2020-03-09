4b445ee2a1737ab5915a6287fd4da1f4db1097bf
sukhpreetn <sukhpreetn@gmail.com>
2020-03-09 07:11:05 -0700

    fix settings file

--- Portal/settings.py
+++ Portal/settings.py
@@ -136,6 +136,6 @@ STATICFILES_DIRS = (
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 MEDIA_URL = '/media/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+#STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 SITE_ID = 2
 LOGIN_REDIRECT_URL = "/pickskill"
